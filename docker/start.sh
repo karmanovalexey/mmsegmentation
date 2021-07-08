2846ba48408fbafc6571df6c3c9c8547cd84c255
karmanovalexey <karmanov.aa@phystech.edu>
2021-07-08 11:03:45 +0300

    gc

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@ docker run \
             --gpus all -it \
             --rm \
             --ipc=host \
-            -v /home/raaicv/mmsegmentation/docker/add_req.sh:/workspace/add_req.sh \
-            -v /data/Mapillary:/workspace/Mapillary \
-            -v /home/raaicv/mmsegmentation:/workspace/mmsegmentation \
+            -v /home/karmanov_aa/mmsegmentation/docker/add_req.sh:/workspace/add_req.sh \
+            -v /home/adeshkin/projects/tools/Mapillary:/workspace/Mapillary \
+            -v /home/karmanov_aa/mmsegmentation:/workspace/mmsegmentation \
             mmseg
---
+++ itl_configs/cgnet_720x720_160k_map.py
@@ -0,0 +1,47 @@
+_base_ = [
+    '../configs/_base_/models/cgnet.py', '../configs/_base_/datasets/mapillary.py',
+    '../configs/_base_/default_runtime.py', './schedules/schedule_160k_adamw.py'
+]
+
+norm_cfg = dict(type='BN', requires_grad=True)
+
+crop_size = (720, 720)
+model = dict(
+    test_cfg=dict(mode='whole'),
+    backbone=dict(norm_cfg=norm_cfg),
+    decode_head=dict(num_classes=66, norm_cfg=norm_cfg))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(1920, 1080), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+# optimizer
+optimizer = dict(type='Adam', lr=0.001, eps=1e-08, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+
+data = dict(
+    samples_per_gpu=3,
+    workers_per_gpu=3,
+    train=dict(pipeline=train_pipeline, data_root='/workspace/Mapillary'),
+    val=dict(data_root='/workspace/Mapillary', split='/workspace/mmsegmentation/splits/split.txt'),
+    test=dict(data_root='/workspace/Mapillary', split='/workspace/mmsegmentation/splits/split.txt')
+)
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='WandbLoggerHook', by_epoch=False),
+        dict(type='TextLoggerHook', by_epoch=False),
+])
